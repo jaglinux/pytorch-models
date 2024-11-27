a1f2bb6d1386c9c56480c814159f2febd51f13f8
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2024-11-26 16:01:37 -0800

    hf manager: add print functions
    Signed-off-by: Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>

--- hf/hf-manager/manager.py
+++ hf/hf-manager/manager.py
@@ -13,17 +13,24 @@ class model:
         self.name = name
         self.model = AutoModelForCausalLM.from_pretrained(name)
         
-    def print(self):
-        print(f" {self.model} ")
+    def print(self, choice):
+        if choice == "basic":
+            print(f" {self.model} ")
+        elif choice == "verbose":
+            print(f" {self.model.__dict__}")
+        else:
+            print("ERROR - model print : wrong choice")
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="hf-models", description="List of models stored in HF, list model details, pre train it, fine tune etc")
     parser.add_argument("--model", help="Provide model name such as gpt2", default="gpt2")
+    parser.add_argument("--print", help="Print model details", choices=("basic", "verbose"))
     args = parser.parse_args()
     if len(sys.argv) == 1:
         # just print gpt2 model
         model = model()
         model.print()
-        
-    
-
+        sys.exit(0)
+    model_param = args.model
+    model = model(model_param)
+    if args.print: model.print(args.print)
