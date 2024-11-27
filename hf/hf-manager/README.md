3906c3ccf5bbee150aeb4efa733da4ccea3e0635
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2024-11-26 17:10:29 -0800

    hf manager: Add support to print model params
    Signed-off-by: Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>

---
+++ hf/hf-manager/README.md
@@ -0,0 +1,8 @@
+# Commands
+python manager.py
+python manager.py --help
+
+python --model gpt2 --print basic
+python --model gpt2 --print verbose
+python --model gpt2 --params
+
--- hf/hf-manager/manager.py
+++ hf/hf-manager/manager.py
@@ -20,11 +20,19 @@ class model:
             print(f" {self.model.__dict__}")
         else:
             print("ERROR - model print : wrong choice")
+
+    def params(self):
+        for k in self.model.state_dict().keys():
+            print("parameter name is ", k)
+            np = self.model.state_dict()[k].detach().numpy()
+            print("shape is ", np.shape)
+            print("numpy values are ", np)
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="hf-models", description="List of models stored in HF, list model details, pre train it, fine tune etc")
     parser.add_argument("--model", help="Provide model name such as gpt2", default="gpt2")
     parser.add_argument("--print", help="Print model details", choices=("basic", "verbose"))
+    parser.add_argument("--params", help="Print model parameters, weights and biases", action="store_true")
     args = parser.parse_args()
     if len(sys.argv) == 1:
         # just print gpt2 model
@@ -34,3 +42,4 @@ if __name__ == "__main__":
     model_param = args.model
     model = model(model_param)
     if args.print: model.print(args.print)
+    if args.params: model.params()
