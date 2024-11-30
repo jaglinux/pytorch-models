00f235b0b6255aa6a4dfa7199ac8d93b1c660a48
Jagadish <jagdish.krishna@gmail.com>
2024-11-29 22:26:38 -0800

    Add aisuite library
    Signed-off-by: Jagadish <jagdish.krishna@gmail.com>

---
+++ aisuite/client.py
@@ -0,0 +1,17 @@
+import aisuite as ai
+client = ai.Client()
+
+models = ["openai:gpt-4o"]
+
+messages = [
+    {"role": "system", "content": "Respond in Pirate English."},
+    {"role": "user", "content": "Tell me a joke."},
+]
+
+for model in models:
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0.75
+    )
+    print(response.choices[0].message.content)
