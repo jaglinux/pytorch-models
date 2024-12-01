714dbf012661a995a2cec3850b57a0efb37ca8f2
Jagadish <jagdish.krishna@gmail.com>
2024-12-01 12:17:13 -0800

    Add Anthropic support
    Signed-off-by: Jagadish <jagdish.krishna@gmail.com>

--- aisuite/client.py
+++ aisuite/client.py
@@ -1,7 +1,7 @@
 import aisuite as ai
 client = ai.Client()
 
-models = ["openai:gpt-4o"]
+models = ["openai:gpt-4o", "anthropic:claude-3-5-sonnet-20240620"]
 
 messages = [
     {"role": "system", "content": "Respond in Pirate English."},
@@ -9,9 +9,11 @@ messages = [
 ]
 
 for model in models:
+    print(f"Seleced model is {model}")
     response = client.chat.completions.create(
         model=model,
         messages=messages,
         temperature=0.75
     )
     print(response.choices[0].message.content)
+    print("------------------------------------------")
